1524e73db615586c147edf142fbb713439dc5ff6
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-03-27 01:17:17 +0200

    styled funciionando

--- medmoderna-front/src/components/Product/ProductCardsListResponsive/ProductCardsListResponsive.tsx
+++ medmoderna-front/src/components/Product/ProductCardsListResponsive/ProductCardsListResponsive.tsx
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import './ProductCardsListResponsive.css';
-import {IProductProps} from "../../../models/IProductProps";
+import styled, { css } from 'styled-components';
+import { IProductProps } from '../../../models/IProductProps';
 import logo from '../../../assets/logo4.png';
 import Modal from 'react-modal';
-import ProductCardNew from "../../Product/ProductCardNew/ProductCardNew";
+import ProductCardNew from '../../Product/ProductCardNew/ProductCardNew';
 import ProductCardDetailResponsive from '../ProductCardDetailResponsive/ProductCardDetailResponsive';
 import { Link } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from 'react-toastify';
 
 type ProductCardsListResponsiveProps = {
     products: IProductProps[];
-}
+};
 
-const ProductCardsListResponsive: React.FC<ProductCardsListResponsiveProps> = ({ products }) => {
+const ShoppingList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  padding: 20px;
+  border-radius: 16px;
+  margin: 0 10%;
+`;
+
+const ProductCardsListResponsive: React.FC<ProductCardsListResponsiveProps> = ({
+                                                                                   products,
+                                                                               }) => {
     const [isMobile, setIsMobile] = useState(window.innerWidth >= 768);
 
     useEffect(() => {
@@ -29,11 +41,11 @@ const ProductCardsListResponsive: React.FC<ProductCardsListResponsiveProps> = ({
 
     return (
         <>
-            <div className="shopping-list">
+            <ShoppingList>
                 {products.map((item, index) => (
                     <ProductCardNew
-                        blur={"3px"}
-                        key={item.name + "_" + Math.floor(Math.random() * 100401).toString()}
+                        blur={'3px'}
+                        key={item.name + '_' + Math.floor(Math.random() * 100401).toString()}
                         imgSrc={item.imgSrc}
                         description={item.description}
                         price={item.price}
@@ -44,11 +56,11 @@ const ProductCardsListResponsive: React.FC<ProductCardsListResponsiveProps> = ({
                         maxLines={2}
                         maxCharsPerLine={70}
                         index={index}
-                       // mobileVersion={isMobile}
+                        // mobileVersion={isMobile}
                         mobileVersion={false}
                     />
                 ))}
-            </div>
+            </ShoppingList>
         </>
     );
 };
