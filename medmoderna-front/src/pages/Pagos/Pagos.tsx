2ff36405ec73ca8ee807cd22bba6cac54565da4b
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-03-28 09:41:35 +0200

    f

--- medmoderna-front/src/pages/Pagos/Pagos.tsx
+++ medmoderna-front/src/pages/Pagos/Pagos.tsx
@@ -5,19 +5,20 @@ import { loadStripe } from '@stripe/stripe-js';
 import Carrito from "../../components/Pago/Carrito";
 import Pago from "../../components/Pago/Pago";
 import PasarelaPago from "../../components/Pago/PasarelaPago";
+import LayoutBase from "../../components/LayoutBase/LayoutBase";
 
 
 const stripePromise = loadStripe('pk_test_51MqQWnLXltkSeDhWSm4OTUnr6dFWUUJaVnW4Bk61q2vbtVxniDyg6rYyXXMmn3ehQeGFWysF0fK7rNqPTMiGk0JB00r5FdEeTm');
 
 const Pagos: React.FC = () => {
     return (
-        <div>
+        <LayoutBase  useBaseAutoHeightHeader={false}>
             <Carrito />
             <Pago />
             <Elements stripe={stripePromise}>
                 <PasarelaPago />
             </Elements>
-        </div>
+        </LayoutBase>
     );
 };
 
