79fc6ef849d390ac3863e3562fef34e765770377
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-08-29 14:53:04 +0200

    a

--- medmoderna-front/src/config.ts
+++ medmoderna-front/src/config.ts
@@ -2,4 +2,4 @@ export const REACT_APP_API_KEY= "KEY";
 export const PROD_URL = "https://medmoderna-api.vercel.app/api";
 export const INT_URL  = "https://medmoderna-api.vercel.app/api";
 export const TESTING_URL = "http://localhost:8080/api";
-export const API_URL = PROD_URL;
+export const API_URL = INT_URL;
